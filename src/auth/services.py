655f034db534fe21e8160aadd563919c13865fb1
HorrorOTB <nicatabdullazade135@gmail.com>
2024-03-07 16:50:46 +0400

    add error handling

--- src/auth/services.py
+++ src/auth/services.py
@@ -1,20 +1,23 @@
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from .utils import create_access_token, create_refresh_token, get_access_token_expires, get_password_hash, get_refresh_token_expires, verify_password, verify_token
+from .utils import create_access_token, create_refresh_token,  get_password_hash, verify_password, verify_access_token
 from ..users.models import ModelUser
 from ..users.responses import UserResponse
 from ..utils.exceptions import user_already_registered_exception, credentials_exception, incorrect_login_exception
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth")
 
 
 async def add_user_db(username: str, password: str):
-    user = ModelUser(username=username, password=password)
-    if user.get_or_none(username=username):
-        raise user_already_registered_exception
-    user.password_hash = get_password_hash(password)
-    user.save()
-    return UserResponse(**user.__data__)
+    try:
+        user = ModelUser(username=username, password=password)
+        if user.get_or_none(username=username):
+            raise user_already_registered_exception
+        user.password_hash = get_password_hash(password)
+        user.save()
+        return UserResponse(**user.__data__)
+    except Exception as e:
+        raise e
 
 
 def authenticate_user(username: str, password: str):
@@ -24,8 +27,8 @@ def authenticate_user(username: str, password: str):
     return user
 
 
-def get_user_by_token(token: str = Depends(oauth2_scheme)):
-    user = verify_token(token)
+async def get_user_by_token(token: str = Depends(oauth2_scheme)):
+    user = verify_access_token(token)
     return user
 
 
@@ -33,8 +36,6 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise incorrect_login_exception
-    access_token_expires = get_access_token_expires()
-    refresh_token_expires = get_refresh_token_expires()
     access_token = create_access_token(
         data={"sub": user.username})
     refresh_token = create_refresh_token(
